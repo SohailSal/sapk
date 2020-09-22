798d28e24913feb246997443b7fde7b13534a2e4
Sohail Saleem <sohails1977@gmail.com>
2020-09-22 23:34:23 +0500

    from home

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /mnt/e/repos/sapk/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /mnt/e/repos/sapk/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /home/sohail/dev/laravel/sapk/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/sohail/dev/laravel/sapk/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
--- resources/views/livewire/home.blade.php
+++ resources/views/livewire/home.blade.php
@@ -2,15 +2,18 @@
 <html>
 <head>
     <title></title>
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <script src="//ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.6.0/dist/alpine.js" defer></script>
     @livewireStyles
 </head>
 <body>
     <div class="container">
+            @livewire('navigation-dropdown')
         <div class="row mt-5 justify-content-center">
             <div class="mt-5 col-md-8">
                 <div class="card">
@@ -25,6 +28,8 @@
 
                         @livewire('customers')
                     </div>
+
+                    <div class="p-5 shadow-lg">Hello Tailwind's CSS</div>
                 </div>
             </div>
         </div>
