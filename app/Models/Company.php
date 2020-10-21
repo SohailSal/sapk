97c87e91af93e8b9c53ad879615fa06248343bf3
Sohail Saleem <sohails1977@gmail.com>
2020-10-21 20:27:02 +0500

    many to many

---
+++ app/Models/Company.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Company extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name','address','email','web','phone','fiscal','incorp','enabled'
+    ];
+
+    public function users()
+    {
+        return $this->belongsToMany('App\Models\User');
+    }
+}
---
+++ database/migrations/2020_10_21_131003_create_companies_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->string('address')->nullable();
+            $table->string('email')->nullable();
+            $table->string('web')->nullable();
+            $table->string('phone')->nullable();
+            $table->enum('fiscal',  ['March', 'June', 'September', 'December'])->default('June');
+            $table->date('incorp')->nullable();
+            $table->tinyInteger('enabled')->default('1');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
