7cfe5ca48922b119916d0e056d785f0f49cb052d
Sohail Saleem <sohails1977@gmail.com>
2020-10-23 10:25:28 +0500

    minor change p-4

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -19,7 +19,7 @@
                     @endcan
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
             <table>
             @foreach(\App\Models\User::all() as $user)
                 <tr>
@@ -32,7 +32,7 @@
     </div>
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-4">
             {{\Auth::user()->id}}
             <br>
             {{\Auth::user()->name}}
