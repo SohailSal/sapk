0f35b93ab801bd2ccea09bbb54aeb65205bdd0e1
Sohail Saleem <sohails1977@gmail.com>
2020-10-27 19:54:07 +0500

    choose company plus

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,31 +5,39 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>SA Accounting</title>
+        <link rel="stylesheet" href="{{ asset('css/app.css') }}">
 
     </head>
     <body>
-        <div class="fixed flex items-top justify-center min-h-screen bg-gray-500 dark:bg-gray-900 sm:items-center sm:pt-0">
+        <div class="fixed flex items-top bg-gray-700 dark:bg-gray-900 pt-0">
+
             @if (Route::has('login'))
-                <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+                <div class="grid grid-cols-3 items-top fixed px-6 py-1">
+                    <div class="">
+                    <a href="{{ url('/') }}" class="text-sm text-white ml-5 hover:text-blue-300">SA Accounting</a>
+                    </div>
+                    <div></div>
+                    <div class="">
                     @auth
-                        <a href="{{ url('/dashboard') }}" class="text-xl text-white underline">Dashboard</a>
+                        <a href="{{ url('/dashboard') }}" class="text-sm text-white ml-5 hover:text-blue-300">Dashboard</a>
                     @else
-                        <a href="{{ route('login') }}" class="text-xl text-white underline">Login</a>
+                        <a href="{{ route('login') }}" class="text-sm text-white ml-5 hover:text-blue-300">Login</a>
 
                         @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 text-xl text-white underline">Register</a>
+                            <a href="{{ route('register') }}" class="text-sm text-white ml-5 hover:text-blue-300">Register</a>
                         @endif
                     @endif
+                    </div>
                 </div>
             @endif
 
             <div class="mx-auto">
-                <div class="flex justify-center pt-12">
-                   <img src="{{asset('/img/boat.jpg')}}">
+                <div class="flex justify-center pt-8">
+                   <img src="{{asset('/img/ledger.jpg')}}" class="max-w-full px-4 ">
                 </div>
 
-                <div class="ml-4 text-center text-sm text-gray-500 sm:text-right sm:ml-0">
-                    Build v{{ Illuminate\Foundation\Application::VERSION }}
+                <div class="ml-4 text-center text-xs text-white sm:text-right sm:ml-0">
+                    Copyright 2020, SA Accounting
                 </div>
             </div>
         </div>
