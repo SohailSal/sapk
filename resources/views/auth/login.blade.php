5c82068f0e7664e151f835f21fef6734c8340064
Sohail Saleem <sohails1977@gmail.com>
2020-11-02 18:12:37 +0500

    input autofill

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -17,7 +17,7 @@
 
             <div>
                 <x-jet-label value="{{ __('Email') }}" />
-                <x-jet-input class="block mt-1 w-full bg-gray-600 text-white rounded leading-tight focus:outline-none focus:shadow-outline" type="email" name="email" :value="old('email')" required autofocus />
+                <x-jet-input class="block mt-1 w-full bg-gray-600 text-white rounded leading-tight focus:outline-none focus:shadow-outline"  type="email" name="email" :value="old('email')" required autofocus />
             </div>
 
             <div class="mt-4">
--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -15,6 +15,22 @@
 
         <!-- Scripts -->
         <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.2.1/dist/alpine.js" defer></script>
+        <style>
+        input:-webkit-autofill,
+input:-webkit-autofill:hover,
+input:-webkit-autofill:focus,
+textarea:-webkit-autofill,
+textarea:-webkit-autofill:hover,
+textarea:-webkit-autofill:focus,
+select:-webkit-autofill,
+select:-webkit-autofill:hover,
+select:-webkit-autofill:focus {
+  border: 3px solid white;
+  -webkit-text-fill-color: white;
+  -webkit-box-shadow: 0 0 0px 1000px #000 inset;
+  transition: background-color 5000s ease-in-out 0s;
+}
+        </style>
     </head>
     <body>
     <div class="flex">
