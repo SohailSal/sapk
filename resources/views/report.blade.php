9e92de3828be2dbb175d69f03feb4c28ff9ea60d
Sohail Saleem <sohails1977@gmail.com>
2020-11-06 10:20:24 +0500

    reports view change

--- resources/views/report.blade.php
+++ resources/views/report.blade.php
@@ -4,9 +4,28 @@
         {{ __('Dashboard') }}
         </h2>
     </x-slot>
+
+
+<div class="py-6 bg-gray-600"">
+    <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+        <div class="overflow-auto sm:rounded-lg  bg-gray-800 shadow-lg px-4 py-4 ">
+            @if (session()->has('message'))
+                <div class="bg-teal-100 border-t-4 border-teal-500 rounded-b text-teal-900 px-4 py-3 shadow-md my-3" role="alert">
+                  <div class="flex">
+                    <div>
+                      <p class="text-sm">{{ session('message') }}</p>
+                    </div>
+                  </div>
+                </div>
+            @endif
+            <div class="flex items-center justify-between">
             <a class="flex-1 border rounded-lg bg-gray-600 p-1 m-2 text-white hover:bg-gray-800" href="{{url('tb')}}">Trial Balance</a>
             <a class="flex-1 border rounded-lg bg-gray-600 p-1 m-2 text-white hover:bg-gray-800" href="{{url('first-chart')}}">Chart</a>
             <a class="flex-1 border rounded-lg bg-gray-600 p-1 m-2 text-white hover:bg-gray-800" href="{{url('excel')}}">Excel</a>
+            </div>
+        </div>
+    </div>
+</div>
 
         <div class="py-12">
             <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
