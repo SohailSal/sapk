c3bf5d4ccb35196ea87c56e33d85036b93adebb7
Sohail Saleem <sohails1977@gmail.com>
2021-01-13 10:53:17 +0500

    table format back

--- resources/views/livewire/sa/accounts.blade.php
+++ resources/views/livewire/sa/accounts.blade.php
@@ -50,9 +50,9 @@
                 </span>
                 <span class="flex-wrap ml-5">{{$accounts->links()}}</span>
             </div>
-            <table class="w-full flex flex-row flex-no-wrap">
+            <table class="w-full">
                 <thead>
-                    <tr class="bg-gray-100 flex flex-col flex-no-wrap sm:table-row" >
+                    <tr class="bg-gray-100" >
                         <th class="px-4 py-1 text-center w-1/6" >Number</th>
                         <th class="px-4 py-1">Name of Account</th>
                         <th class="px-4 py-1">Group of Account</th>
@@ -61,7 +61,7 @@
                 </thead>
                 <tbody>
                     @foreach($accounts as $account)
-                    <tr class="text-white flex flex-col flex-no-wrap sm:table-row"  >
+                    <tr class="text-white"  >
                         <td class="border px-4 py-1" >{{ $account->number }}</td>
                         <td class="border px-4 py-1" >{{ $account->name }}</td>
                         <td class="border px-4 py-1">{{ $account->groupName }}</td>
