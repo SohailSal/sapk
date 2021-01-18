52386f10d73957d4717ac6e5cbbc49fb88d72dd2
Sohail Saleem <sohails1977@gmail.com>
2021-01-18 17:52:52 +0500

    almost complete ui

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -31,23 +31,21 @@
             @endif
 
             <div class="">
-                <div class="flex pt-8 h-11/12">
-                   <img src="{{asset('/img/ledger.jpg')}}" class="px-2 w-full object-cover flex-wrap">
+                <div class="flex pt-8 h-11/12 z-10">
+                   <img src="{{asset('/img/ledger.jpg')}}" class="px-2 w-full object-cover">
+                   <div class="absolute z-20 invisible md:visible top-32 md:left-10 lg:left-60">
+                        <svg id="svg8" width="178mm" height="55mm" version="1.1" viewBox="0 0 190 33.611" xmlns="http://www.w3.org/2000/svg">
+                            <g id="layer1" transform="translate(-8.6366 -101.9)">
+                                <rect id="rect10" x="9.6366" y="102.9" width="23.397" height="29.612" fill="none" stroke="#000" stroke-width="2"/>
+                                <text id="text837" x="15.38962" y="130.41719" font-family="'Matura MT Script Capitals'" font-size="24.976px" fill="#000" stroke="#000" stroke-width="1" style="line-height:1.25" xml:space="preserve"><tspan id="tspan835" x="15.38962" y="130.41719" font-family="sans-serif" font-size="24.976px" stroke-width=".3">S</tspan></text>
+                                <rect id="rect10-5" x="33.034" y="102.9" width="23.397" height="29.612" fill="none" stroke="#000" stroke-width="2"/>
+                                <text id="text837-2" x="34.55349" y="130.41719" font-family="'Matura MT Script Capitals'" font-size="24.976px" fill="#000" stroke="#000" stroke-width="1" style="line-height:1.25" xml:space="preserve"><tspan id="tspan835-5" x="34.55349" y="130.41719" font-family="sans-serif" font-size="24.976px" stroke-width=".3">A</tspan></text>
+                                <text id="text837-2-1" x="59.926598" y="130.34967" font-family="'Matura MT Script Capitals'" font-size="24.976px" fill="#000" stroke="#000" stroke-width="1" style="line-height:1.25" xml:space="preserve"><tspan id="tspan835-5-6" x="59.926598" y="130.34967" font-family="sans-serif" font-size="24.976px" stroke-width=".3">accounting</tspan></text>
+                            </g>
+                        </svg>
+                    </div>
                 </div>
-                <div class="hidden absolute z-9 inset-y-52 inset-x-72 rounded-lg p-10 bg-white h-44 w-7/12 opacity-50 shadow-lg">
-                </div>
-                <div class="hidden absolute z-10 inset-y-40 inset-x-72 rounded-lg p-10">
-                    <svg id="svg8" width="178mm" height="55mm" version="1.1" viewBox="0 0 190 33.611" xmlns="http://www.w3.org/2000/svg">
-                    <g id="layer1" transform="translate(-8.6366 -101.9)">
-                    <rect id="rect10" x="9.6366" y="102.9" width="23.397" height="29.612" fill="none" stroke="#000" stroke-width="2"/>
-                    <text id="text837" x="15.38962" y="130.41719" font-family="'Matura MT Script Capitals'" font-size="24.976px" fill="#000" stroke="#000" stroke-width="1" style="line-height:1.25" xml:space="preserve"><tspan id="tspan835" x="15.38962" y="130.41719" font-family="sans-serif" font-size="24.976px" stroke-width=".3">S</tspan></text>
-                    <rect id="rect10-5" x="33.034" y="102.9" width="23.397" height="29.612" fill="none" stroke="#000" stroke-width="2"/>
-                    <text id="text837-2" x="34.55349" y="130.41719" font-family="'Matura MT Script Capitals'" font-size="24.976px" fill="#000" stroke="#000" stroke-width="1" style="line-height:1.25" xml:space="preserve"><tspan id="tspan835-5" x="34.55349" y="130.41719" font-family="sans-serif" font-size="24.976px" stroke-width=".3">A</tspan></text>
-                    <text id="text837-2-1" x="59.926598" y="130.34967" font-family="'Matura MT Script Capitals'" font-size="24.976px" fill="#000" stroke="#000" stroke-width="1" style="line-height:1.25" xml:space="preserve"><tspan id="tspan835-5-6" x="59.926598" y="130.34967" font-family="sans-serif" font-size="24.976px" stroke-width=".3">accounting</tspan></text>
-                    </g>
-                    </svg>
-                </div>
-                <div class="mt-3 text-center text-xs text-white">
+                <div class="mt-3 text-center text-xs text-white invisible md:visible">
                     Copyright 2020, SA accounting - beta 0.2.11
                 </div>
             </div>
